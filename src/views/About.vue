fca463f58d44671b549a8b3750765d656e4e3f03
lentoo <729533020@qq.com>
2019-01-17 09:07:15 +0800

    :bugs: fix document is not defined

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <welcome name="about"></welcome>
   </div>
 </template>
+<script>
+import Welcome from '@/components/Welcome'
+export default {
+  name: 'ViewAbout',
+  components: {
+    Welcome
+  }
+}
+</script>
+<style module>
+  .about {
+    color: red;
+  }
+</style>
